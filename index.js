9ad66c24674d1201ab7750e077a063feb47a20c7
Pushpesh987 <krpushpesh108@gmail.com>
2024-03-01 16:12:09 +0530

    added googleauth

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express'); 
+const app = express(); 
+const session = require('express-session');
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET 
+}));
+
+
+const userRoutes = require('./routes/userRoute');
+
+app.use('/',userRoutes);
+
+app.listen(3000 , () => { 
+	console.log("Server Running on port 3000"); 
+});
